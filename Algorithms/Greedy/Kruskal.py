6f26078b565f0c74e921bb81d7119b7877d40abe
Wen Junhua <wenjh1998@gmail.com>
2022-04-21 16:42:28 -0400

    fix: fix and refactor mst algorithms

--- Algorithms/Greedy/Kruskal.py
+++ Algorithms/Greedy/Kruskal.py
@@ -1,35 +1,10 @@
-graph = {
-    'A': {'B': 5, 'D': 5, 'E': 7},
-    'B': {'A': 5, 'C': 4, 'D': 8, 'E': 2},
-    'C': {'B': 4, 'D': 8, 'F': 4},
-    'D': {'A': 5, 'B': 8, 'C': 8, 'F': 9, 'E': 3},
-    'E': {'A': 7, 'B': 2, 'D': 3, 'F': 2},
-    'F': {'C': 4, 'D': 9, 'E': 2}
-}
-
-source = 'A'
-edge_list = []
-
-# Convert AL to Edge list
-for key, value in graph.items():
-    for key2, value2 in value.items():
-        edge_list.append((value2, key, key2))
-
-# Sort the edgelist
-edge_list.sort(reverse=True)
-
-# Create the UFDS
-ufds = {}
-
-
 def get_top_parent(ufds: dict, edge1: str) -> str:
     if edge1 not in ufds:
-       return edge1
+        return edge1
 
     curr = ufds[edge1]
     while curr in ufds:
         curr = ufds[curr]
-
 
     # Compression
     ufds[edge1] = curr
@@ -37,27 +12,46 @@ def get_top_parent(ufds: dict, edge1: str) -> str:
     return curr
 
 
-def add_as_parent(ufds: dict, child:str, parent:str) -> None:
+def add_as_parent(ufds: dict, child: str, parent: str) -> None:
     ufds[child] = parent
 
-mst = []
 
-# Start the kruskal algorithm
-while len(edge_list):
-    value, from_edge, to_edge = edge_list.pop()
-    from_parent = get_top_parent(ufds, from_edge)
-    to_parent = get_top_parent(ufds, to_edge)
+def kruskals(graph: dict[str, dict[str, int]]) -> list:
+    # Create the UFDS
+    edge_list = []
 
-    if from_parent == to_parent:
-        continue
+    # Convert AL to Edge list
+    for key, value in graph.items():
+        for key2, value2 in value.items():
+            edge_list.append((value2, key, key2))
 
-    mst.append((from_edge, to_edge, value))
-    add_as_parent(ufds, from_parent, to_parent)
+    # Sort the edgelist
+    edge_list.sort(reverse=True)
+    ufds = {}
+    mst = []
 
-print(mst)
+    # Start the kruskal algorithm
+    while len(edge_list):
+        value, from_edge, to_edge = edge_list.pop()
+        from_parent = get_top_parent(ufds, from_edge)
+        to_parent = get_top_parent(ufds, to_edge)
+
+        if from_parent == to_parent:
+            continue
+
+        mst.append((from_edge, to_edge, value))
+        add_as_parent(ufds, from_parent, to_parent)
+    return mst
 
 
-
-    
-
-
+if __name__ == '__main__':
+    graph = {
+        'A': {'B': 5, 'D': 5, 'E': 7},
+        'B': {'A': 5, 'C': 4, 'D': 8, 'E': 2},
+        'C': {'B': 4, 'D': 8, 'F': 4},
+        'D': {'A': 5, 'B': 8, 'C': 8, 'F': 9, 'E': 3},
+        'E': {'A': 7, 'B': 2, 'D': 3, 'F': 2},
+        'F': {'C': 4, 'D': 9, 'E': 2}
+    }
+    result = kruskals(graph)
+    print(result)
--- Algorithms/Greedy/Prims.py
+++ Algorithms/Greedy/Prims.py
@@ -1,36 +1,47 @@
 import heapq
 
-graph = {
-    'A': {'B': 5, 'D': 5, 'E': 7},
-    'B': {'A': 5, 'C': 4, 'D': 8, 'E': 2},
-    'C': {'B': 4, 'D': 8, 'F': 4},
-    'D': {'A': 5, 'B': 8, 'C': 8, 'F': 9, 'E': 3},
-    'E': {'A': 7, 'B': 2, 'D': 3, 'F': 2},
-    'F': {'C': 4, 'D': 9, 'E': 2}
-}
 
-source = 'A'
-arr = list(map(lambda x: (x[1],source,x[0]), graph['A'].items()))
-heapq.heapify(arr)
-visited = set('A')
-edges = []
+def prims(graph: dict[str, dict[str, int]]) -> list:
+    """Prim's algorithm"""
 
-while len(visited) < len(graph):
-    # Pop the top
-    weight, from_edge, to_edge = heapq.heappop(arr)
+    source = list(graph.keys())[0]
+    arr = list(map(lambda x: (x[1], source, x[0]), graph[source].items()))
+    heapq.heapify(arr)
+    visited = set(source)
+    edges = []
 
-    # Check if edge is visited
-    if to_edge in visited:
-        continue
+    if len(arr) == 1:
+        weight, from_edge, to_edge = heapq.heappop(arr)
+        edges.append((from_edge, to_edge, weight))
+        return edges
 
-    # Add edge to tree
-    visited.add(to_edge)
-    edges.append((from_edge, to_edge, weight))
+    while len(visited) < len(graph):
+        # Pop the top
+        weight, from_edge, to_edge = heapq.heappop(arr)
 
-    # Add Adjacent edges
-    for new_to_edge, weight in graph[to_edge].items():
-        new_add = (weight, to_edge, new_to_edge)
-        heapq.heappush(arr, new_add)
+        # Check if edge is visited
+        if to_edge in visited:
+            continue
+
+        # Add edge to tree
+        visited.add(to_edge)
+        edges.append((from_edge, to_edge, weight))
+
+        # Add Adjacent edges
+        for new_to_edge, weight in graph[to_edge].items():
+            new_add = (weight, to_edge, new_to_edge)
+            heapq.heappush(arr, new_add)
+    return edges
 
 
-print(edges)
+if __name__ == '__main__':
+    graph = {
+        'A': {'B': 5, 'D': 5, 'E': 7},
+        'B': {'A': 5, 'C': 4, 'D': 8, 'E': 2},
+        'C': {'B': 4, 'D': 8, 'F': 4},
+        'D': {'A': 5, 'B': 8, 'C': 8, 'F': 9, 'E': 3},
+        'E': {'A': 7, 'B': 2, 'D': 3, 'F': 2},
+        'F': {'C': 4, 'D': 9, 'E': 2}
+    }
+    result = prims(graph)
+    print(result)
